b2dedc3b10f87ad95005d297da67d0a41841b816
Damian <damjeronzalez23@gmail.com>
2023-07-18 03:21:24 -0500

    fix index property error

--- src/components/RandomUserList.tsx
+++ src/components/RandomUserList.tsx
@@ -55,7 +55,6 @@ export default function RandomUserList({
               user={user}
               key={user.email}
               handleDelete={handleDelete}
-              index={index}
             />
           )
         })}
